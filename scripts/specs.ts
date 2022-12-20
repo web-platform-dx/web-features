5f0cfc8ba7709a875e8824361fc77d29507b28a3
Philip Jägenstedt <philip@foolip.org>
2022-12-20 11:14:36 +0100

    Check feature spec URL against web-specs
    Closes https://github.com/web-platform-dx/feature-set/issues/12.

---
+++ scripts/specs.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import webSpecs from 'web-specs' assert { type: 'json' };
+
+import features from '../index.js';
+
+const specUrls = new Set(webSpecs.map(spec => spec.nightly.url));
+
+let checked = 0;
+let errors = 0;
+
+for (const [id, data] of Object.entries(features)) {
+    const url = new URL(data.spec);
+    // Remove any #fragment from end of URL.
+    url.hash = '';
+    if (!specUrls.has(url.toString())) {
+        console.error(`URL for ${id} not in web-specs: ${url}`);
+        errors++;
+    }
+    checked++;
+}
+
+if (errors) {
+    console.log(`\n${checked} features checked, found ${errors} error(s)`);
+    process.exit(1);
+} else {
+    console.log(`${checked} features checked, no errors`);
+}
