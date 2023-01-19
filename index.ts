dba5eba4bf2ee6cc6301423000a4095567d780e7
Daniel D. Beck <daniel@ddbeck.com>
2023-01-19 16:37:02 +0100

    Generate JSON Schema definitions from types

--- index.ts
+++ index.ts
@@ -4,18 +4,37 @@ import path from 'path';
 import { fdir } from 'fdir';
 import YAML from 'yaml';
 
-interface FeatureData {
-    spec: string,
-    caniuse?: string,
-    status?: SupportStatus
+/** Web platform feature */
+export interface FeatureData {
+    /** Specification URL
+     * @format uri
+    */
+    spec: string;
+    /** caniuse.com identifier */
+    caniuse?: string;
+    /** Whether a feature is considered a "baseline" web platform feature and when it achieved that status */
+    status?: SupportStatus;
+    /** Sources of support data for this feature */
     compat_features?: string[];
+    /** Usage stats */
+    usage_stats?: usage_stats_url | [usage_stats_url, usage_stats_url, ...usage_stats_url[]];  // A single URL or an array of two or more
 }
 
 type browserIdentifier = "chrome" | "firefox" | "safari";
 
 interface SupportStatus {
-    is_baseline: boolean, since?: string, support?: {[K in browserIdentifier]?: string}
+    /** Whether the feature achieved baseline status */
+    is_baseline: boolean;
+    /** Date the feature achieved baseline status */
+    since?: string;
+    /** Browser versions that most-recently introduced the feature */
+    support?: {[K in browserIdentifier]?: string};
 }
+
+/** Usage stats URL
+ * @format uri
+*/
+type usage_stats_url = string;
 
 // Some FeatureData keys aren't (and may never) be ready for publishing.
 // They're not part of the public schema (yet).
