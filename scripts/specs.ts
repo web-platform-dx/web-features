d716ba3e7aa9855d474338132f8bf00ce8185903
Daniel D. Beck <daniel@ddbeck.com>
2023-03-31 17:04:25 +0200

    Allow specs to be subpaths of a base spec URL

--- scripts/specs.ts
+++ scripts/specs.ts
@@ -1,18 +1,37 @@
+import assert from "node:assert/strict";
+
 import webSpecs from 'web-specs' assert { type: 'json' };
 
 import features from '../index.js';
 
-const specUrls = new Set(webSpecs.map(spec => spec.nightly.url));
+const specUrls = webSpecs.map(spec => new URL(spec.nightly.url));
+
+function isOK(url: URL) {
+    for (const specUrl of specUrls) {
+        if (specUrl.protocol !== url.protocol || specUrl.origin !== url.origin) {
+            continue;
+        }
+        if (url.pathname.startsWith(specUrl.pathname)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function testIsOK() {
+    assert.ok(isOK(new URL("https://tc39.es/ecma262/multipage/")));
+    assert.ok(isOK(new URL("https://tc39.es/ecma262/multipage/indexed-collections.html#sec-array.prototype.at")));
+    assert.ok(!isOK(new URL("https://typo.csswg.org/css-anchor-position-1/#anchoring")));
+};
+testIsOK();
 
 let checked = 0;
 let errors = 0;
 
 for (const [id, data] of Object.entries(features)) {
     const url = new URL(data.spec);
-    // Remove any #fragment from end of URL.
-    url.hash = '';
-    if (!specUrls.has(url.toString())) {
-        console.error(`URL for ${id} not in web-specs: ${url}`);
+    if (!isOK(url)) {
+        console.error(`URL for ${id} not in web-specs: ${url.toString()}`);
         errors++;
     }
     checked++;
