55862cd209017cda0a12fba025072b13e8ac56c7
Daniel D. Beck <daniel@ddbeck.com>
2023-04-17 13:26:07 +0100

    Avoid exception terminology
    Co-authored-by: Philip Jägenstedt <philip@foolip.org>

--- scripts/specs.ts
+++ scripts/specs.ts
@@ -6,15 +6,15 @@ import features from '../index.js';
 
 const specUrls = webSpecs.map(spec => new URL(spec.nightly.url));
 
-type exception = [url: string, message: string];
-const exceptions: exception[] = [
+type allowlistItem = [url: string, message: string];
+const defaultAllowlist: allowlistItem[] = [
     [
         "https://wicg.github.io/navigation-api/",
         "This spec is moving to WHATWG HTML. Remove this exception when https://github.com/whatwg/html/pull/8502 merges."
     ],
 ];
 
-function isOK(url: URL, allowlist: exception[] = exceptions) {
+function isOK(url: URL, allowlist: allowlistItem[] = defaultAllowlist) {
     for (const specUrl of specUrls) {
         if (specUrl.protocol !== url.protocol || specUrl.origin !== url.origin) {
             continue;
