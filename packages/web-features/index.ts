87bba1572ac9fd4c212ee715535740b79895a40c
Daniel D. Beck <daniel@ddbeck.com>
2023-05-04 16:01:16 +0100

    Specify node imports
    Co-authored-by: Philip Jägenstedt <philip@foolip.org>

--- packages/web-features/index.ts
+++ packages/web-features/index.ts
@@ -1,5 +1,5 @@
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
+import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
 
 const jsonPath = fileURLToPath(new URL("./index.json", import.meta.url));
 const features = JSON.parse(readFileSync(jsonPath, { encoding: "utf-8" }));
