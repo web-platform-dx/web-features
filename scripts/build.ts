4668fba2a84e424dd0ac94a27d20dd8262e5471f
Daniel D. Beck <daniel@ddbeck.com>
2023-08-11 18:53:16 +0200

    Move LICENSE.txt to the root of the project

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,11 +4,15 @@ import stringify from 'fast-json-stable-stringify';
 
 import features from '../index.js';
 
+const rootDir = new URL('..', import.meta.url);
+const packageDir = new URL('./packages/web-features/', rootDir);
+
 function build() {
     const json = stringify(features);
     // TODO: Validate the resulting JSON against a schema.
-    const path = new URL('../packages/web-features/index.json', import.meta.url);
+    const path = new URL('index.json', packageDir);
     fs.writeFileSync(path, json);
+    fs.copyFileSync(new URL("LICENSE.txt", rootDir), new URL("LICENSE.txt", packageDir));
 }
 
 build();
