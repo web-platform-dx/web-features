5294f36b74d141d9ce0be6e6d3a53c73d0a8a31d
Daniel D. Beck <daniel@ddbeck.com>
2024-01-25 12:23:04 +0100

    Generate .d.ts file for published package

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,16 +3,23 @@ import fs from 'fs';
 import stringify from 'fast-json-stable-stringify';
 
 import features from '../index.js';
+import { execSync } from 'child_process';
 
 const rootDir = new URL('..', import.meta.url);
 const packageDir = new URL('./packages/web-features/', rootDir);
+
+const filesToCopy = ["LICENSE.txt", "types.ts"];
 
 function build() {
     const json = stringify(features);
     // TODO: Validate the resulting JSON against a schema.
     const path = new URL('index.json', packageDir);
     fs.writeFileSync(path, json);
-    fs.copyFileSync(new URL("LICENSE.txt", rootDir), new URL("LICENSE.txt", packageDir));
+    for (const file of filesToCopy) {
+        fs.copyFileSync(new URL(file, rootDir), new URL(file, packageDir));
+    }
+    execSync("npm install", { cwd: "./packages/web-features"});
+    execSync("npm run prepare", { cwd: "./packages/web-features"});
 }
 
 build();
