a06b34e488c7e3921ad9b5b70c9381c5412860f4
Philip Jägenstedt <philip@foolip.org>
2024-02-15 13:53:09 +0700

    Add COLRv1 color font format

--- scripts/specs.ts
+++ scripts/specs.ts
@@ -17,6 +17,10 @@ const defaultAllowlist: allowlistItem[] = [
     //     "https://example.com/spec/",
     //     "Allowed because…. Remove this exception when https://example.com/org/repo/pull/1234 merges."
     // ]
+    [
+        "https://learn.microsoft.com/en-us/typography/opentype/otspec191alpha/colr",
+        "Allowed because COLRv1 is shipping in Chrome. Remove this exception when https://github.com/w3c/browser-specs/issues/1209 is resolved."
+    ]
 ];
 
 function isOK(url: URL, allowlist: allowlistItem[] = defaultAllowlist) {
