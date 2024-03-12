e8c6c1d6a5f5684a5ba348a84e74c2ac1a396f14
Daniel D. Beck <daniel@ddbeck.com>
2024-03-12 12:55:21 +0100

    Demo snapshot testing

--- packages/web-features-figuring/src/baseline/index.test.ts
+++ packages/web-features-figuring/src/baseline/index.test.ts
@@ -1,10 +1,24 @@
 import assert from "node:assert/strict";
 import { readFileSync } from "node:fs";
 
+import bcd from "@mdn/browser-compat-data";
+import * as chai from "chai";
+import chaiJestSnapshot from "chai-jest-snapshot";
+
 import { computeBaseline } from ".";
 import { Compat } from "../browser-compat-data/compat";
 
+chai.use(chaiJestSnapshot);
+
 describe("computeBaseline", function () {
+  before(function () {
+    chaiJestSnapshot.resetSnapshotRegistry();
+  });
+
+  beforeEach(function () {
+    chaiJestSnapshot.configureUsingMochaContext(this);
+  });
+
   it("returns a result for a feature", function () {
     const result = computeBaseline({
       compatKeys: ["css.properties.border-color"],
@@ -16,7 +30,7 @@ describe("computeBaseline", function () {
   it("returns a result for a feature (data provided)", function () {
     const compat = new Compat(
       JSON.parse(
-        readFileSync(new URL("scrubbed.test.json", import.meta.url), {
+        readFileSync(new URL("index.test.ts.bcd.json", import.meta.url), {
           encoding: "utf-8",
         }),
       ),
@@ -28,6 +42,25 @@ describe("computeBaseline", function () {
         checkAncestors: false,
       },
       compat,
+    );
+    assert.equal(result.baseline, "high");
+  });
+
+  it("tests via a BCD snapshot", function () {
+    // If we thin out the data first, the snapshot assertion can run much faster
+    const data = {
+      __meta: bcd.__meta,
+      browsers: bcd.browsers,
+      css: bcd.css,
+    };
+    chai.expect(data).to.matchSnapshot(true);
+
+    const result = computeBaseline(
+      {
+        compatKeys: ["css.properties.border-color"],
+        checkAncestors: false,
+      },
+      new Compat(data),
     );
     assert.equal(result.baseline, "high");
   });
