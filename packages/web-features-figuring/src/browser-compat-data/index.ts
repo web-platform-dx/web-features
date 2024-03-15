d80cbfb8ffd4c794f5502765d609de720520cca9
Daniel D. Beck <daniel@ddbeck.com>
2024-03-15 13:23:19 +0100

    Fix TODO about explicit re-exports

--- packages/web-features-figuring/src/browser-compat-data/index.ts
+++ packages/web-features-figuring/src/browser-compat-data/index.ts
@@ -1,11 +1,25 @@
-// TODO: explicit reexports
-export { browser, Browser } from "./browser";
-export { feature, Feature } from "./feature";
-export { query } from "./query";
-export { Release } from "./release";
-export {
+import { browser, Browser } from "./browser";
+import { Compat } from "./compat";
+import { feature, Feature } from "./feature";
+import { query } from "./query";
+import { Release } from "./release";
+import {
+  RealSupportStatement,
   statement,
   SupportStatement,
-  RealSupportStatement,
 } from "./supportStatements";
-export { walk } from "./walk";
+import { walk } from "./walk";
+
+export {
+  browser,
+  Browser,
+  Compat,
+  feature,
+  Feature,
+  query,
+  RealSupportStatement,
+  Release,
+  statement,
+  SupportStatement,
+  walk,
+};
