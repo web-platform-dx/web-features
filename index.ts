d288544e168435c5ed08ccb28d882a65a64f76ef
Daniel D. Beck <daniel@ddbeck.com>
2024-03-28 18:09:54 +0100

    Rename to `compute-baseline`

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import { Temporal } from '@js-temporal/polyfill';
 
 // TODO: Use npm workspaces (or similar) strategy to reference the package,
 // instead of reaching into the source like this
-import { BASELINE_LOW_TO_HIGH_DURATION } from "./packages/web-features-figuring/src/constants"
+import { BASELINE_LOW_TO_HIGH_DURATION } from "./packages/compute-baseline/src/constants"
 
 // The longest description allowed, to avoid them growing into documentation.
 const descriptionMaxLength = 280;
