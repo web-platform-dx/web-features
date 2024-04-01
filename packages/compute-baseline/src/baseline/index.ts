fcbb923e3be80f7122f51d5a5263bc4706953b2f
Daniel D. Beck <daniel@ddbeck.com>
2024-04-01 21:21:45 +0200

    Use compute-baseline as dependency
    This modifies how `compute-baseline` exposes its low-to-high constant
    (and other primary functions), then consumes that from the top-level
    `package.json` and `index.ts`. This is table-setting for consuming the
    `compute-baseline` package, to generate statuses.

--- packages/compute-baseline/src/baseline/index.ts
+++ packages/compute-baseline/src/baseline/index.ts
@@ -10,6 +10,12 @@ import { browsers } from "./core-browser-set";
 import { isFuture, toDateString, toHighDate } from "./date-utils";
 import { support } from "./support";
 
+// Number of months after Baseline low that Baseline high happens. Keep in sync with definition:
+// https://github.com/web-platform-dx/web-features/blob/main/docs/baseline.md#wider-support-high-status
+export const BASELINE_LOW_TO_HIGH_DURATION = Temporal.Duration.from({
+  months: 30,
+});
+
 interface FeatureSelector {
   compatKeys: [string, ...string[]];
   checkAncestors: boolean;
--- packages/compute-baseline/src/constants.ts
+++
@@ -1,7 +0,0 @@
-import { Temporal } from "@js-temporal/polyfill";
-
-// Number of months after Baseline low that Baseline high happens. Keep in sync with definition:
-// https://github.com/web-platform-dx/web-features/blob/main/docs/baseline.md#wider-support-high-status
-export const BASELINE_LOW_TO_HIGH_DURATION = Temporal.Duration.from({
-  months: 30,
-});
