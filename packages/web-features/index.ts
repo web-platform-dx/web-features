5f1ae1954f8f54359ffb90e8aaeaf58ba05db7ae
Daniel D. Beck <daniel@ddbeck.com>
2024-04-03 21:31:53 +0100

    Make .prettierignore opt-ins easier to apply (#780)
    As written before, it opted-out directories too. This makes adding more files to the configuration a bit of a pain: you have to opt-in the directory first.

--- packages/web-features/index.ts
+++ packages/web-features/index.ts
@@ -4,6 +4,8 @@ import { fileURLToPath } from "node:url";
 import { FeatureData } from "./types";
 
 const jsonPath = fileURLToPath(new URL("./index.json", import.meta.url));
-const features = JSON.parse(readFileSync(jsonPath, { encoding: "utf-8" })) as { [id: string]: FeatureData };
+const features = JSON.parse(readFileSync(jsonPath, { encoding: "utf-8" })) as {
+  [id: string]: FeatureData;
+};
 
 export default features;
