d698961cd2052cf85c2e2a25840b59e4a52bebab
Philip Jägenstedt <philip@foolip.org>
2024-05-30 21:35:35 +0200

    Add a type to resolve a TypeScript red squiggly

--- scripts/dist.ts
+++ scripts/dist.ts
@@ -246,7 +246,7 @@ function warnOnNeedlessOverrides(id, overridden, generated) {
 
 function main() {
   // Map from .yml to .dist.yml to filter out duplicates.
-  const sourceToDist = new Map(
+  const sourceToDist: Map<string, string> = new Map(
     argv.filenames.map((filePath: string) => {
       let { dir, name, ext } = path.parse(filePath);
       if (ext !== ".yml") {
