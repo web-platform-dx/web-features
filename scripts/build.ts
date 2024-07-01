0791475f4cf1cc0b6d1c61ada0c117870e01e2bf
Daniel D. Beck <daniel@ddbeck.com>
2024-07-01 16:18:46 +0200

    Apply prettier to build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,25 +1,31 @@
-import fs from 'fs';
+import fs from "fs";
 
-import stringify from 'fast-json-stable-stringify';
+import stringify from "fast-json-stable-stringify";
 
-import features from '../index.js';
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
+import features from "../index.js";
 
-const rootDir = new URL('..', import.meta.url);
-const packageDir = new URL('./packages/web-features/', rootDir);
+const rootDir = new URL("..", import.meta.url);
+const packageDir = new URL("./packages/web-features/", rootDir);
 
 const filesToCopy = ["LICENSE.txt", "types.ts"];
 
 function build() {
-    const json = stringify(features);
-    // TODO: Validate the resulting JSON against a schema.
-    const path = new URL('index.json', packageDir);
-    fs.writeFileSync(path, json);
-    for (const file of filesToCopy) {
-        fs.copyFileSync(new URL(file, rootDir), new URL(file, packageDir));
-    }
-    execSync("npm install", { cwd: "./packages/web-features",  encoding: "utf-8"});
-    execSync("npm run prepare", { cwd: "./packages/web-features", encoding: "utf-8"});
+  const json = stringify(features);
+  // TODO: Validate the resulting JSON against a schema.
+  const path = new URL("index.json", packageDir);
+  fs.writeFileSync(path, json);
+  for (const file of filesToCopy) {
+    fs.copyFileSync(new URL(file, rootDir), new URL(file, packageDir));
+  }
+  execSync("npm install", {
+    cwd: "./packages/web-features",
+    encoding: "utf-8",
+  });
+  execSync("npm run prepare", {
+    cwd: "./packages/web-features",
+    encoding: "utf-8",
+  });
 }
 
 build();
