4f8bfe8cbbb318a398fe747d22d41438d9aa8831
Philip Jägenstedt <philip@foolip.org>
2024-07-19 17:48:57 +0200

    Rename schema file to data.schema.json and publish it (#1348)
    This will include the schema in the NPM package. The idea is to also
    publish it with GitHub releases.
    
    Co-authored-by: Daniel D. Beck <daniel@ddbeck.com>

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,7 @@
-import { execSync } from "child_process";
+import { basename } from "node:path";
+import { execSync } from "node:child_process";
+import fs from "node:fs";
 import stringify from "fast-json-stable-stringify";
-import fs from "fs";
 import yargs from "yargs";
 import * as data from "../index.js";
 
@@ -17,14 +18,17 @@ yargs(process.argv.slice(2))
 
 function buildPackage() {
   const packageDir = new URL("./packages/web-features/", rootDir);
-  const filesToCopy = ["LICENSE.txt", "types.ts"];
+  const filesToCopy = ["LICENSE.txt", "types.ts", "schemas/data.schema.json"];
 
   const json = stringify(data);
   // TODO: Validate the resulting JSON against a schema.
   const path = new URL("data.json", packageDir);
   fs.writeFileSync(path, json);
   for (const file of filesToCopy) {
-    fs.copyFileSync(new URL(file, rootDir), new URL(file, packageDir));
+    fs.copyFileSync(
+      new URL(file, rootDir),
+      new URL(basename(file), packageDir),
+    );
   }
   execSync("npm install", {
     cwd: "./packages/web-features",
