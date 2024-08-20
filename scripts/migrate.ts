e013fffa147c1d289b9c13c892d753403b4e40c9
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-08-19 23:07:28 -0700

    Use node: prefix in imports

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,6 +1,6 @@
-import assert from 'assert';
-import fs from 'fs';
-import path from 'path';
+import assert from 'node:assert';
+import fs from 'node:fs';
+import path from 'node:path';
 import { fdir } from 'fdir';
 import { features } from '../index.js';
 
