9e380e35589b69a5751e9a00dcb1cfab1cd7ea0e
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-08-20 22:05:44 -0700

    Opt-in to Prettier formatting (seriously? Why is this opt-*in*...?)

--- scripts/migrate-to-bcd.ts
+++ scripts/migrate-to-bcd.ts
@@ -1,9 +1,9 @@
-import assert from 'node:assert';
-import fs from 'node:fs';
-import path from 'node:path';
-import {fileURLToPath} from "node:url";
-import { fdir } from 'fdir';
-import { features } from '../index.js';
+import assert from "node:assert";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { fdir } from "fdir";
+import { features } from "../index.js";
 
 const BCD_PATH = process.env.BCD_PATH
   ? path.resolve(process.env.BCD_PATH)
@@ -15,76 +15,80 @@ const bcdToFeature = new Map();
 // Set of the first part of the BCD path encountered.
 const bcdDirs = new Set();
 
-for (const [feature, {compat_features}] of Object.entries(features)) {
-    if (!compat_features) {
-        continue;
-    }
-    for (const key of compat_features) {
-        assert(!bcdToFeature.has(key), `${key} is used in compat_features twice`);
-        bcdToFeature.set(key, feature);
-        bcdDirs.add(key.split('.')[0]);
-    }
+for (const [feature, { compat_features }] of Object.entries(features)) {
+  if (!compat_features) {
+    continue;
+  }
+  for (const key of compat_features) {
+    assert(!bcdToFeature.has(key), `${key} is used in compat_features twice`);
+    bcdToFeature.set(key, feature);
+    bcdDirs.add(key.split(".")[0]);
+  }
 }
 
 const bcdJsons = new fdir()
-    .withBasePath()
-    .filter((fp) => {
-        const dir = path.relative(BCD_PATH, fp).split(path.sep)[0];
-        return bcdDirs.has(dir);
-    })
-    .filter((fp) => fp.endsWith('.json'))
-    .crawl(BCD_PATH)
-    .sync();
+  .withBasePath()
+  .filter((fp) => {
+    const dir = path.relative(BCD_PATH, fp).split(path.sep)[0];
+    return bcdDirs.has(dir);
+  })
+  .filter((fp) => fp.endsWith(".json"))
+  .crawl(BCD_PATH)
+  .sync();
 
 const lookup = (root, key) => {
-    const parts = key.split('.');
-    let node = root;
-    for (const part of parts) {
-        if (Object.hasOwn(node, part)) {
-            node = node[part];
-        } else {
-            return undefined;
-        }
+  const parts = key.split(".");
+  let node = root;
+  for (const part of parts) {
+    if (Object.hasOwn(node, part)) {
+      node = node[part];
+    } else {
+      return undefined;
     }
-    return node;
+  }
+  return node;
 };
 
 for (const fp of bcdJsons) {
-    const src = fs.readFileSync(fp, { encoding: 'utf-8' });
-    const data = JSON.parse(src);
-    let updated = false;
-    for (const [key, feature] of bcdToFeature.entries()) {
-        const node = lookup(data, key);
-        if (!node || !node.__compat) {
-            continue;
-        }
-        bcdToFeature.delete(key);
-
-        const tag = `web-features:${feature}`;
-        const compat = node.__compat;
-        if (compat.tags?.includes(tag)) {
-            continue;
-        }
-
-        const index = compat.tags?.findIndex(t => t.startsWith("web-features:") && !(t.startsWith("web-features:snapshot:")));
-        if (index > -1) {
-            // Remove any other web-features tags -- features can't be a part of multiple groups, right?
-            compat.tags.pop(index);
-        }
-
-        if (compat.tags) {
-            compat.tags.push(tag);
-        } else {
-            compat.tags = [tag];
-        }
-        updated = true;
+  const src = fs.readFileSync(fp, { encoding: "utf-8" });
+  const data = JSON.parse(src);
+  let updated = false;
+  for (const [key, feature] of bcdToFeature.entries()) {
+    const node = lookup(data, key);
+    if (!node || !node.__compat) {
+      continue;
     }
-    if (updated) {
-        const src = JSON.stringify(data, null, '  ') + '\n';
-        fs.writeFileSync(fp, src, { encoding: 'utf-8' })
+    bcdToFeature.delete(key);
+
+    const tag = `web-features:${feature}`;
+    const compat = node.__compat;
+    if (compat.tags?.includes(tag)) {
+      continue;
     }
+
+    const index = compat.tags?.findIndex(
+      (t) =>
+        t.startsWith("web-features:") &&
+        !t.startsWith("web-features:snapshot:"),
+    );
+    if (index > -1) {
+      // Remove any other web-features tags -- features can't be a part of multiple groups, right?
+      compat.tags.pop(index);
+    }
+
+    if (compat.tags) {
+      compat.tags.push(tag);
+    } else {
+      compat.tags = [tag];
+    }
+    updated = true;
+  }
+  if (updated) {
+    const src = JSON.stringify(data, null, "  ") + "\n";
+    fs.writeFileSync(fp, src, { encoding: "utf-8" });
+  }
 }
 
 for (const [key, feature] of bcdToFeature) {
-    console.warn('Not migrated:', feature, key);
+  console.warn("Not migrated:", feature, key);
 }
