8d9cb42f8c3b7f6e1011c8ae614964229de41478
James Stuckey Weber <james@oddbird.net>
2024-11-24 22:01:12 -0500

    Add static API

---
+++ gh-pages/src/api/api.11tydata.js
@@ -0,0 +1,7 @@
+module.exports = async function () {
+    const { features } = await import('../../../packages/web-features/index.js');
+    return {
+        features: Object.keys(features).map(id => ({ ...features[id], id })),
+        featureList: { features: Object.keys(features) },
+    };
+}
