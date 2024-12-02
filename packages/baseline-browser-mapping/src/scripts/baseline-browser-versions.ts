98eb80759ca2e3d5830fb77658f3a7207ce4f76e
conwayt <conwayt@google.com>
2024-12-02 15:35:16 +0000

    fix throw syntax error

--- packages/baseline-browser-mapping/src/scripts/baseline-browser-versions.ts
+++ packages/baseline-browser-mapping/src/scripts/baseline-browser-versions.ts
@@ -49,7 +49,7 @@ const compareVersions = (
     previousVersionString.split(".");
 
   if (!incomingVersionStringMajor || !previousVersionStringMajor) {
-    throw (
+    throw Error(
       "One of these version strings is broken: " +
       incomingVersionString +
       " or " +
