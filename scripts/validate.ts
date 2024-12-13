434a842cdffff47ab5a00936c98fdcd0e6eaaa84
Daniel D. Beck <daniel@ddbeck.com>
2024-12-13 16:21:09 +0100

    Stop ignoring errors from ajv

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -4,18 +4,16 @@ import assert from "node:assert/strict";
 
 import * as schema from "../schemas/data.schema.json" with { type: "json" };
 
-export function validate(data: any) {
-  const ajv = new Ajv({ allErrors: true, allowUnionTypes: true });
-  addFormats(ajv);
-  // TODO: turn on strictNullChecks, fix all the errors, and replace this with:
-  // const validator = ajv.compile<WebFeaturesData>(schema);
-  const validator = ajv.compile(schema);
+const ajv = new Ajv({ allErrors: true, allowUnionTypes: true });
+addFormats(ajv);
+// TODO: turn on strictNullChecks, fix all the errors, and replace this with:
+// const validator = ajv.compile<WebFeaturesData>(schema);
+const validator = ajv.compile(schema);
 
-  assert.equal(
-    validator({}),
-    false,
-    "Failed confidence check: schema validates empty object",
-  );
+assert.equal(
+  validator({}),
+  false,
+  "Failed confidence check: schema validates empty object",
+);
 
-  return validator;
-}
+export { validator as validate };
