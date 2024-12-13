d079b45496df7282b590be4e230326f3c3556715
Daniel D. Beck <daniel@ddbeck.com>
2024-12-13 16:44:21 +0100

    Remove `uri` fromat annotation from spec URLs' JSON Schema

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -1,11 +1,9 @@
 import Ajv from "ajv";
-import addFormats from "ajv-formats";
 import assert from "node:assert/strict";
 
 import * as schema from "../schemas/data.schema.json" with { type: "json" };
 
 const ajv = new Ajv({ allErrors: true, allowUnionTypes: true });
-addFormats(ajv);
 // TODO: turn on strictNullChecks, fix all the errors, and replace this with:
 // const validator = ajv.compile<WebFeaturesData>(schema);
 const validator = ajv.compile(schema);
