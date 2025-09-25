696a9e9cd5aaef04f06a8fbddd84e6d50a26f3e1
Patrick Brosset <patrick.brosset@microsoft.com>
2025-09-25 16:56:28 +0200

    Allow the explainer

--- scripts/specs.ts
+++ scripts/specs.ts
@@ -141,6 +141,10 @@ const defaultAllowlist: allowlistItem[] = [
     [
         "https://www.w3.org/TR/2022/WD-selectors-4-20220507/#the-target-within-pseudo",
         "Allowed because this is where the feature last appeared in the spec before removal."
+    ],
+    [
+        "https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/Accessibility/AriaNotify/explainer.md",
+        "Allowed because the ariaNotify() method is not yet in a formal spec. Remove this exception when a formal spec is available."
     ]
 ];
 
