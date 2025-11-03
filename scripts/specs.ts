b0ef7b7f9c16520be37e000238e9168b7b106563
Patrick Brosset <patrickbrosset@gmail.com>
2025-11-03 11:42:50 -0500

    Add the referencetarget feature (#3376)
    Co-authored-by: Daniel D. Beck <daniel@ddbeck.com>

--- scripts/specs.ts
+++ scripts/specs.ts
@@ -135,6 +135,14 @@ const defaultAllowlist: allowlistItem[] = [
         "Allowed because this is where the feature last appeared in the spec before removal."
     ],
     [
+        "https://github.com/whatwg/dom/pull/1353",
+        "Allowed because this is where the referencetarget feature is being added to the DOM spec"
+    ],
+    [
+        "https://github.com/whatwg/html/pull/10995",
+        "Allowed because this is where the referencetarget feature is being added to the HTML spec"
+    ],
+    [
         "https://patcg-individual-drafts.github.io/topics/",
         "Allowed because the Topics API isn't on a standards track yet. Remove this exception when it is."
     ],
