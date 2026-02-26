7f089d5da9a5fba72f380a4a0ff904509b392e2e
Niklas Merz <niklasmerz@apache.org>
2026-02-26 19:14:30 +0100

    compute webview

--- packages/compute-baseline/src/baseline/core-browser-set.ts
+++ packages/compute-baseline/src/baseline/core-browser-set.ts
@@ -10,6 +10,16 @@ export const identifiers = [
   "safari_ios",
 ];
 
+export const webViewIdentifiers = ["webview_android", "webview_ios"];
+
 export function browsers(compat: Compat) {
   return identifiers.map((b) => compat.browser(b));
+}
+
+export function webViews(compat: Compat) {
+  return webViewIdentifiers.map((b) => compat.browser(b));
+}
+
+export function allBrowsers(compat: Compat) {
+  return [...browsers(compat), ...webViews(compat)];
 }
