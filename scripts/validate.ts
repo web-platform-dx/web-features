9aa62fa790c2ffaf5de10e704a71bf880ede2c09
Philip Jägenstedt <philip@foolip.org>
2026-03-30 10:51:41 +0200

    Publish data.proposed.json
    Add html-pis as a first proposed feature to show that it works.

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -2,6 +2,7 @@ import Ajv from "ajv";
 import assert from "node:assert/strict";
 
 import * as schema from "../schemas/data.schema.json" with { type: "json" };
+import * as proposedSchema from "../schemas/data.proposed.schema.json" with { type: "json" };
 
 const ajv = new Ajv({ allErrors: true, allowUnionTypes: true });
 // TODO: turn on strictNullChecks, fix all the errors, and replace this with:
@@ -14,4 +15,18 @@ assert.equal(
   "Failed confidence check: schema validates empty object",
 );
 
-export { validator as validate };
+const proposedValidator = ajv.compile(proposedSchema);
+
+assert.equal(
+  proposedValidator({}),
+  false,
+  "Failed confidence check: schema validates empty object",
+);
+
+assert.equal(
+  proposedValidator({ features: {} }),
+  true,
+  "Failed confidence check: schema rejects empty features object",
+);
+
+export { validator as validate, proposedValidator as validateProposed };
