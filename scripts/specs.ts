b9a758e456940f15aa93350a4a385e54a945ae79
Daniel D. Beck <daniel@ddbeck.com>
2026-05-22 15:48:41 +0200

    Add `protected-audience` and `ad-selection` features (#2635)
    Co-authored-by: Patrick Brosset <patrickbrosset@gmail.com>

--- scripts/specs.ts
+++ scripts/specs.ts
@@ -231,6 +231,14 @@ const defaultAllowlist: allowlistItem[] = [
     [
         "https://github.com/WICG/PEPC/blob/main/usermedia_element.md",
         "Replace with https://w3c.github.io/mediacapture-extensions/#the-usermedia-html-element when https://github.com/w3c/mediacapture-extensions/pull/168 merges."
+    ],
+    [
+        "https://github.com/WICG/privacy-preserving-ads",
+        "Discontinued spec allowed for discouraged feature."
+    ],
+    [
+        "https://wicg.github.io/turtledove/",
+        "Discontinued spec allowed for discouraged feature."
     ]
 ];
 
